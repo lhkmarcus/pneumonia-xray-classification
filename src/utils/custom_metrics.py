23f98f82d7f2a635ed9b39b0cec2be4f02669669
lhkmarcus <lim.marcus.hk@gmail.com>
2023-07-18 13:41:05 +0800

    Changes to custom metrics callback class

--- src/utils/custom_metrics.py
+++ src/utils/custom_metrics.py
@@ -5,7 +5,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
 from sklearn.metrics import f1_score, roc_auc_score
-from sklearn.metrics import recall_score, precision_score
 from sklearn.metrics import matthews_corrcoef
 
 
@@ -20,13 +19,9 @@ class CustomCallback(tf.keras.callbacks.Callback):
         # Instantiate macro-averaged metrics
         self._mcc = tf.keras.metrics.Metric("mcc")
         self._aucroc = tf.keras.metrics.Mean("macro_aucroc")
-        self._recall = tf.keras.metrics.Mean("macro_recall")
         self._fmeasure = tf.keras.metrics.Mean("macro_fmeasure")
-        self._precision = tf.keras.metrics.Mean("macro_precision")
 
         self.epoch = 0
-        self.summary_writer = tf.summary.create_file_writer(
-            os.path.join(log_dir, model.name))
 
     def on_epoch_end(self, batch, logs=None):
         self.epoch += 1
@@ -34,44 +29,18 @@ class CustomCallback(tf.keras.callbacks.Callback):
 
         self._mcc.reset_state()
         self._aucroc.reset_state()
-        self._recall.reset_state()
         self._fmeasure.reset_state()
-        self._precision.reset_state()
 
         mcc = matthews_corrcoef(
             self.y_val, np.argmax(predictions, axis=-1))
         fmeasure = f1_score(
-            self.y_val, np.argmax(predictions, axis=-1), average=None)
-        recall = recall_score(
-            self.y_val, np.argmax(predictions, axis=-1), average=None)
-        precision = precision_score(
             self.y_val, np.argmax(predictions, axis=-1), average=None)
         aucroc = roc_auc_score(
             self.y_val, np.argmax(predictions, axis=-1), average=None)
 
         self._mcc.update_state(mcc)
         self._aucroc.update_state(aucroc)
-        self._recall.update_state(recall)
         self._fmeasure.update_state(fmeasure)
-        self._precision.update_state(precision)
 
-        self._write_metric(
-            self._mcc.name,
-            self._mcc.result().numpy().astype(float))
-        self._write_metric(
-            self._aucroc.name,
-            self._aucroc.result().numpy().astype(float))
-        self._write_metric(
-            self._recall.name,
-            self._recall.result().numpy().astype(float))
-        self._write_metric(
-            self._fmeasure.name,
-            self._fmeasure.result().numpy().astype(float))
-        self._write_metric(
-            self._precision.name,
-            self._precision.result().numpy().astype(float))
-
-    def _write_metric(self, name, value):
-        with self.summary_writer.as_default():
-            tf.summary.scalar(name, value, step=self.epoch)
-            self.summary_writer.flush()
+        # Append the values to the weights and biases log:
+        
